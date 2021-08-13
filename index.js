e01f6dd8fb1021a4df8de998ac4f1ad55574d513
Hashem Gray <Hgray1109@gmail.com>
2021-08-13 19:05:32 -0400

    commit tree ec656be4d421eb6dfd6c9205dc7e71f1b2d8aea2

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
+let num1 = 2
+let num2 = 31
+let multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(0.5);
+
+let num3 = 16
+let num4 = 4
+let mod = num3 / num4 
+
+const max = Math.max(1, 2, 4, 20)
